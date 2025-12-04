8cf29c76694f59de4530c142c5da9ee60278991b
gargi301234 <gargi7348@gmail.com>
2025-12-04 13:50:18 +0530

    Add files via upload

---
+++ areaofcircle.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int r;
+    cout<<"enter the diameter=";
+    cin>>r;
+    double pi=3.14;
+    double area=pi*r;
+    cout<<area<<endl;
+}
